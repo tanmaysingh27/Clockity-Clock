85e57d8fd29b0153f53bdd15d34cf790a7c13dd7
tanmaysingh27 <111690894+tanmaysingh27@users.noreply.github.com>
2023-11-22 22:29:55 +0530

    file name change
    Changed the fie name for better understanding

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -6,8 +6,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define ssid "TanmayNord"
-#define password "wifi1234"
+#define ssid "Host Name"
+#define password "wifiPassword"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
